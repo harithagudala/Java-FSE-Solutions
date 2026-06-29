74c883e7d24fafed218986d371537e985ee1c2c4
Haritha <harithagudala9@gmail.com>
2026-06-29 23:22:46 +0530

    Added JUnit Exercise 4 AAA Pattern

---
+++ JUnitAAAPatternExample/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(5, result);
+    }
+}
